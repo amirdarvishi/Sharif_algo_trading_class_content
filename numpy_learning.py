03842ab9abce63f50ebd88cb48239df5dc01976c
amir.darvish <amir.darvish@gmail.com>
2022-10-11 14:45:58 +0330

    edit code and add some usable comments

--- numpy_learning.py
+++ numpy_learning.py
@@ -1,34 +1,39 @@
 import numpy as np
 
+# define two array 
 a = np.array([1,3,5], dtype='int16')
 b = np.array([[1,5,9,10],[2,6,7,9]])
-# print(a)
-print(b)
-# Dimension
-# print('dimension of a array: ',a.ndim)
-# print('dimension of b array: ',b.ndim)
 
-# # size
-# print(a.itemsize)
-# print(a.nbytes)
+# print arrays
+print(a)
+print(b)
+
+# Dimension
+print('dimension of a array: ',a.ndim)
+print('dimension of b array: ',b.ndim)
+
+# size
+print(a.itemsize)
+print(a.nbytes)
 
 # print specific element
-# print(b[1,1])
-# #specific row 
-# print(b[1, :])
-# # column 
-# print(b[: ,2])
+print(b[1,1])
 
-# initialize 
-# zero_matrix = np.zeros((5,5))
-# print(zero_matrix)
+# specific row 
+print(b[1, :])
+# column 
+print(b[: ,2])
 
+# initialize an array filled with zero
+zero_matrix = np.zeros((5,5))
+print(zero_matrix)
 
-# ones_matrix = np.ones((5,5))
-# print(ones_matrix)
+# initialize an array filled with one 
+ones_matrix = np.ones((5,5))
+print(ones_matrix)
 
-# ones_matrix = np.full((5,5),23)
-# print(ones_matrix)
+ones_matrix = np.full((5,5),23)
+print(ones_matrix)
 
 random = np.random.rand(4,2)
 print(random)
@@ -37,10 +42,10 @@ random_int = np.random.randint(1,23, size=(4,6))
 print(random_int)
 
 
-# # math
-# print(b)
-# # print(a + 2)
-# print(np.sum(b,axis=1))
+# math
+print(b)
+print(a + 2)
+print(np.sum(b,axis=1))
 
 
 # stacking 
